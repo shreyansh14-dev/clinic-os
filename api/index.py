7152b36e42f4779cb2d2f5953175fdffbae120c1
shreyansh14-dev <shreyansh14@somaiya.edu>
2026-08-21 16:44:05 +0530

    Delete api directory

--- api/index.py
+++
@@ -1,7 +0,0 @@
-﻿import sys
-import os
-
-# Add root directory to sys.path so app.py and database.py can be imported anywhere
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import app
